98952e0e632a1eb451c6c9cbdfd61d0281458a87
github_username_here <email@example.com>
2025-07-27 21:17:01 +0200

    xd

--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
 def get_book_text(file):
-    with open(file) as f:
+    with open(file, encoding="utf-8", errors="ignore") as f:
         file_contents = f.read()
         file_contents = file_contents.lower()
         litery = set(file_contents)
--- total.py
+++ total.py
@@ -1,5 +1,5 @@
 def get_book_total(file):
-    with open(file) as f:
+    with open(file, encoding="utf-8", errors="ignore") as f:
         file_contents = f.read()
         words = file_contents.split()
         number = len(words)
