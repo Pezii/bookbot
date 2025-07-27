52b1f19e16da8aad05f314295d40efc604bab7bf
Pezii <patryk.janusiewiczf1@gmail.com>
2025-07-27 16:00:48 +0200

    franken

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(file):
+    with open(file) as f:
+        file_contents = f.read()
+    return(file_contents)
+
+def main():
+    xd = get_book_text("/books/frankenstein.txt")
+    print(xd)
